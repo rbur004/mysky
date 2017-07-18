fe654b83f0c871905c428ec2f1bcf0168a36dcb2
robertburrowes <r.burrowes@auckland.ac.nz>
2017-07-18 16:53:18 +1200

    Playing to see what commands work

--- test.rb
+++ test.rb
@@ -3,45 +3,32 @@ require_relative 'sky_commands.rb'
 
 sky_cli = SkyCommands.new
 sky_cli.getMediaInfo
-exit 0
+#sky_cli.getMediaInfo_Ext #No more useful data than getMediaInfo
+#sky_cli.getTransportInfo
+#sky_cli.getTransportSettings
+#sky_cli.getPositionInfo
+sky_cli.listPresets #Only FactoryDefaults.
+#sky_cli.getTranportationActions #Play,Stop,Pause
+#sky_cli.channel(number: 7) #BBC TV
+#sky_cli.channel(name: 'Prime') #Channel 4
+#sky_cli.stop #No effect if on TV, but stops a running file playing and drops back to TV
 
-#sky_cli.play_recording(uri: "file://pvr/29012F89") #Fails
-#sky_cli.browseRecordTasks #Fails
-#sky_cli.channel(number: 7) #Sky Movies
-#sky_cli.stop
-sky_cli.getTranportationActions
-
-sky_cli.rewind(rate: 30)
-sleep 3
-sky_cli.ff(rate: 30)
-sleep 5
-
-sky_cli.getMediaInfo
-sleep 3
-sky_cli.channel(number: 30) #Sky Movies
-sky_cli.getMediaInfo
-sleep 3
-sky_cli.channel(name: 'Prime') #Channel 4
-sky_cli.getMediaInfo
-
-
-
-#sky_cli.play_recording(name: "http://10.2.2.196:80/mp4/new/Victoria.mp4") #Fails with 500
-
-#
+#sky_cli.rewind(rate: 30)
+#sleep 5
+#sky_cli.ff(rate: 30)
+#sleep 5
 #sky_cli.play
 #sky_cli.stop
 
+#All these fail
+#sky_cli.setMute #Fails. Unimplemented.
+#sky_cli.setMute(mute: 0) #Fails. Unimplemented.
+#sky_cli.selectPreset(presetName: "FactoryDefaults") #fails with 701 error
+#sky_cli.selectPreset(presetName: "InstallationDefaults")  #fails with 701 error
 #sky_cli.browseRecordTasks(filter:'', startingIndex:0, requestedCount:10, sortCriteria:'')
-#sky_cli.getPositionInfo
-#sky_cli.getMediaInfo_Ext
-#sky_cli.getMediaInfo
-#sky_cli.play_recording(name: "file://pvr/29012F89")
-#sky_cli.play_recording(name: "file://pvr/29013D47")
-#sky_cli.getMediaInfo
-#sky_cli.play_recording(name: "/pvr/29013D47")
+#sky_cli.play_recording(name: "file://pvr/29012F89") #Fails
+#sky_cli.play_recording(uri: "http://10.2.2.196:80/mp4/new/Victoria.mp4") #Fails.
 #sky_cli.browseRecordTasks
-#sky_cli.channel(number: 30)
 #sky_cli.getSystemUpdateID
 #sky_cli.getFeatureList
 #sky_cli.getBrowseSortCapabilities
