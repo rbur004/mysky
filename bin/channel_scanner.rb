c2dffb0ae5fb52e160f67402a28924413b0173c7
robertburrowes <r.burrowes@auckland.ac.nz>
2017-07-21 10:00:18 +1200

    Scan for unknown channels

---
+++ bin/channel_scanner.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require_relative '../rlib/sky_commands.rb'
+
+sky_cli = SkyCommands.new
+
+#Look for channels not in my channel list
+channels = MySky_Channels.new
+(0..0xFFFF).each do |i|
+  code = "%X" % i
+  if channels.isvalid?(code: code) == false && code != '29B' && code != "298" #these require remote 'cancel' to continue.
+    response = sky_cli.channel(code: code, quiet: true)
+    if response.code.to_i == 200
+      puts code
+    end
+  end
+end
